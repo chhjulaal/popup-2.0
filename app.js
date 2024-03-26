65dd5ef1c8eb0a98c42cb214cbd308b540114f02
Madhur <147310449+chhjulaal@users.noreply.github.com>
2024-03-26 19:12:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let ok = document.getElementById("ok");
 ok.addEventListener("click", function () {
   let b = document.querySelector(".box");
+  console.log(this);
   this.parentElement.style.display = "none";
+  let sbmtBtn = document.getElementById("btn");
+  sbmtBtn.style.display = "block";
 });
 let sbmtBtn = document.getElementById("btn");
 
 sbmtBtn.addEventListener("click", function () {
   let bb = document.querySelector(".box");
   bb.style.display = "inline-block";
+  sbmtBtn.style.display = "none";
 });
